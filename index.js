922002c5bb7d8397498e3ef343a257ca0111074b
Daniel Cotton <dan@daniel-cotton.co.uk>
2017-04-23 15:47:02 +0100

    removed block-scoped declarations to support older node versions

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const uaParser = require('ua-parser');
-const debug = require('debug')('differential-serve');
+var uaParser = require('ua-parser');
+var debug = require('debug')('differential-serve');
 
 /**
  * Construct a user-agent parsing  middleware given two strings
@@ -8,7 +8,7 @@ const debug = require('debug')('differential-serve');
  */
 module.exports = function (es2015dir = 'es2015', es5dir = 'es5') {
 
-  const edgeUAParse = /(?:\b(MS)?IE\s+|\bTrident\/7\.0;.*\s+rv:|\bEdge\/)(\d+)/;
+  var edgeUAParse = /(?:\b(MS)?IE\s+|\bTrident\/7\.0;.*\s+rv:|\bEdge\/)(\d+)/;
    
   /**
    * Parse a user-agent and return the base-directory 
@@ -17,16 +17,16 @@ module.exports = function (es2015dir = 'es2015', es5dir = 'es5') {
    * @param es5dir - the directory name/path for es5 code (from base path)
    * @private 
    */
-  const parseUA = (ua, es2015dir, es5dir) => {
-    let edgeUA = ua.match(edgeUAParse);
+  function parseUA(ua, es2015dir, es5dir) {
+    var edgeUA = ua.match(edgeUAParse);
     if (edgeUA) {
       edgeUA = edgeUA[0].split('/');
     }
-    let uaParsed = uaParser.parseUA(ua);
-    let osParsed = uaParser.parseOS(ua);
-    const browser = uaParsed.family;
-    const majorVersion = uaParsed.major;
-    const supportsES2015 = (edgeUA && parseInt(edgeUA[1]) >= 14) ||
+    var uaParsed = uaParser.parseUA(ua);
+    var osParsed = uaParser.parseOS(ua);
+    var browser = uaParsed.family;
+    var majorVersion = uaParsed.major;
+    var supportsES2015 = (edgeUA && parseInt(edgeUA[1]) >= 14) ||
       (!edgeUA && ((browser === 'Chrome' && majorVersion >= 49) ||
       (browser === 'Chrome Mobile' && majorVersion >= 49) ||
       (browser === 'Safari' && majorVersion >= 10) ||
